d7913bd6ced2e73efed26a4a3c2b222c1f62f619
Brett <brett_neighbors@yahoo.com>
2023-12-02 14:32:59 -0700

    Edited 10 stars test

--- tests/requests.test.js
+++ tests/requests.test.js
@@ -34,8 +34,15 @@ describe("requests.js", () => {
     });
 
     it("should return a list of constellations with fewer than 10 stars with planets", async () => {
-      // Write code here
-      expect(1).toBe(2);
+      jest.spyOn(axios, "get");
+      axios.get.mockImplementation(() => Promise.resolve({ data }));
+    
+      const response = await index();
+    
+      const expected = data.slice(0, 2);
+      expect(response).toEqual(expected);
+    
+      jest.clearAllMocks();
     });
 
     it("should log an error to the console", async () => {
@@ -45,14 +52,3 @@ describe("requests.js", () => {
   });
 });
 
-it("should return a list of constellations with fewer than 10 stars with planets", async () => {
-  jest.spyOn(axios, "get");
-  axios.get.mockImplementation(() => Promise.resolve({ data }));
-
-  const response = await index();
-
-  const expected = data.slice(0, 2);
-  expect(response).toEqual(expected);
-
-  jest.clearAllMocks();
-});
